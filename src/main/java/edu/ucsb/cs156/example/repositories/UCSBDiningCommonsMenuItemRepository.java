ed7714280f987d236f84f6d162f993f35a5cf627
Longjiao Chen <longjiao@ucsb.edu>
2025-10-27 13:42:26 -0700

    Fix code format issues for CI

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemRepository.java
@@ -4,6 +4,9 @@ import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItem;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-/** The UCSBDiningCommonsMenuItemRepository is a repository for UCSBDiningCommonsMenuItem entities. */
+/**
+ * The UCSBDiningCommonsMenuItemRepository is a repository for UCSBDiningCommonsMenuItem entities.
+ */
 @Repository
-public interface UCSBDiningCommonsMenuItemRepository extends CrudRepository<UCSBDiningCommonsMenuItem, Long> {}
+public interface UCSBDiningCommonsMenuItemRepository
+    extends CrudRepository<UCSBDiningCommonsMenuItem, Long> {}
